32b0a88018ab752fdc35f5d925442d2e0665eb18
ChromaNyan <nyancatz593@gmail.com>
2025-03-22 20:34:07 -0500

    fix: make energized steel tag entries optional
    doesn't functionally break anything on its own, but would throw an error and cause LMFT to throw a fit

--- src/main/java/com/chromanyan/chromaticconstruct/datagen/CCBlockTagProvider.java
+++ src/main/java/com/chromanyan/chromaticconstruct/datagen/CCBlockTagProvider.java
@@ -4,11 +4,11 @@ import com.chromanyan.chromaticconstruct.ChromaticConstruct;
 import com.chromanyan.chromaticconstruct.init.CompatTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import owmii.powah.block.Blcks;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -19,6 +19,6 @@ public class CCBlockTagProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
-        tag(CompatTags.Blocks.STORAGE_BLOCKS_ENERGIZED_STEEL).add(Blcks.ENERGIZED_STEEL.get());
+        tag(CompatTags.Blocks.STORAGE_BLOCKS_ENERGIZED_STEEL).addOptional(new ResourceLocation("powah", "energized_steel_block"));
     }
 }
--- src/main/java/com/chromanyan/chromaticconstruct/datagen/CCItemTagProvider.java
+++ src/main/java/com/chromanyan/chromaticconstruct/datagen/CCItemTagProvider.java
@@ -5,11 +5,11 @@ import com.chromanyan.chromaticconstruct.init.CompatTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import owmii.powah.item.Itms;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -22,6 +22,6 @@ public class CCItemTagProvider extends ItemTagsProvider {
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
         copy(CompatTags.Blocks.STORAGE_BLOCKS_ENERGIZED_STEEL, CompatTags.Items.STORAGE_BLOCKS_ENERGIZED_STEEL);
-        tag(CompatTags.Items.INGOTS_ENERGIZED_STEEL).add(Itms.ENERGIZED_STEEL.get());
+        tag(CompatTags.Items.INGOTS_ENERGIZED_STEEL).addOptional(new ResourceLocation("powah", "steel_energized"));
     }
 }
