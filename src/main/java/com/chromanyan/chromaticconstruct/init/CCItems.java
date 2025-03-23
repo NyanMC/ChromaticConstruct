6e0ea20f9531af11c5150632c758e800630eb97e
ChromaNyan <nyancatz593@gmail.com>
2025-03-22 21:13:16 -0500

    refactor: unused imports

--- src/main/java/com/chromanyan/chromaticconstruct/init/CCItems.java
+++ src/main/java/com/chromanyan/chromaticconstruct/init/CCItems.java
@@ -2,12 +2,8 @@ package com.chromanyan.chromaticconstruct.init;
 
 import com.chromanyan.chromaticconstruct.ChromaticConstruct;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import slimeknights.mantle.registration.object.ItemObject;
 import slimeknights.tconstruct.common.registration.ItemDeferredRegisterExtension;
-import slimeknights.tconstruct.shared.TinkerCommons;
 
 public class CCItems {
 
